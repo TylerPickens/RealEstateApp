8fc5678f3a7439d28b79af931a963ab95cca92be
TylerPickens <tapickens1@buffs.wtamu.edu>
2020-05-04 13:44:00 -0500

    App changes

--- app/src/main/java/edu/wtamu/cis/realestateapp/MainActivity.kt
+++ app/src/main/java/edu/wtamu/cis/realestateapp/MainActivity.kt
@@ -2,11 +2,27 @@ package edu.wtamu.cis.realestateapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.renderscript.ScriptGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : FragmentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val inputFragment = InputFragment()
+
+        setCurrentFragment(inputFragment)
+
     }
+
+    private fun setCurrentFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fragmentContainer, fragment)
+            commit()
+        }
 }
--- app/src/main/res/InputFragment.kt
+++ app/src/main/res/InputFragment.kt
@@ -7,12 +7,15 @@ import android.os.Parcelable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import edu.wtamu.cis.realestateapp.R.id.inputCity
 import edu.wtamu.cis.realestateapp.R.layout.input_fragment
+import kotlinx.android.synthetic.main.input_fragment.*
 
-class InputFragment() : Fragment(), Parcelable {
 
-    var isEmpty = true
+@Suppress("UNREACHABLE_CODE")
+class InputFragment() : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,15 +25,40 @@ class InputFragment() : Fragment(), Parcelable {
         val view = inflater?.inflate(R.layout.input_fragment, container, false)
 
         return view
+
+        val searchButton: Button? = view?.findViewById(R.id.searchButton)
+        searchButton?.setOnClickListener{ v: View -> onButtonClick(v)}
+
+
     }
 
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
+    private fun validate() :Boolean{
+        if(inputState.text.toString().isEmpty()) {
+            inputState.error = "State should be entered as a two character code"
+            return false
+        }else if(inputCity.text.toString().isEmpty()){
+            inputCity.error = "Please enter a city"
+            return false
+        }else if(inputPrice.toString().isEmpty()) {
+            inputPrice.error = "Please enter a maximum price"
+            return false
+        }else if(inputBedrooms.toString().isEmpty()) {
+            inputPrice.error = "Please enter the number of bedrooms you want"
+            return false
+        }else if(inputBathrooms.toString().isEmpty()) {
+            inputBathrooms.error = "Please enter the number of bathrooms you want"
+            return false
+        }
 
+        return true
     }
 
-    override fun describeContents(): Int {
-        return 0
-    }
+    private fun onButtonClick(v: View) {
+        when(v?.id) {
+            R.id.searchButton->{
 
+            }
+        }
+    }
 
 }
